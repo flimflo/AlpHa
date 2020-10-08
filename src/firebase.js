fcb6ab295e6e4c8b17d2c08de8254a7cc8caf41a
chava <salvador.barboza.portillo@gmail.com>
2020-10-08 16:54:08 -0500

    Add firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAAR0pacHEPgv6wp7mbfaitwS1bysjHZDY",
+    authDomain: "alphaserver-459f1.firebaseapp.com",
+    databaseURL: "https://alphaserver-459f1.firebaseio.com",
+    projectId: "alphaserver-459f1",
+    storageBucket: "alphaserver-459f1.appspot.com",
+    messagingSenderId: "200962155216",
+    appId: "1:200962155216:web:509779fafccabbe199b4d5"
+  };
+
+
+firebase.initializeApp(firebaseConfig)
