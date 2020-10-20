a786592fbab1186e9b4156d4f4a3e7477bb826c6
Fernando Limón <fernando@kinedu.com>
2020-10-20 13:02:35 -0500

    Regulation Module Added

--- src/components/pages/regulation/Regulation.js
+++ src/components/pages/regulation/Regulation.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
+import { useDocumentDataOnce } from "react-firebase-hooks/firestore"
+import { useParams } from "react-router-dom"
+import { LeaguesCollection, RulesCollection } from "../../../firestoreCollections"
+import "../regulation/Regulation.css"
 
 function Regulation() {
+  const {leagueId} = useParams()
+  const [value, loading, error] = useDocumentDataOnce(LeaguesCollection.doc("DMpXPRsBI9DuOm8gOA7y")) //leagueId)
+  let [rules, setRules] = useState([])
+
+  async function getRules() {
+    let rulesList = []
+    await RulesCollection.where("leagueId", "==", "DMpXPRsBI9DuOm8gOA7y") //leagueId)
+    .get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+             for (let i = 0; i < doc.data().rules.length; i++) {
+                 rulesList.push(doc.data().rules[i].rule)
+             }
+        })
+    })
+    setRules(rulesList)
+    console.log(rulesList)
+}
+
+useEffect(() => {
+  getRules()
+}, []);
+
   return(
     <>
-      <h1>Reglamento</h1>
+    <div className="Title">
+    <h1>Reglamento oficial de la liga {value?.leagueName}</h1>
+    </div>
+    <div className="Regulation">
+        {rules.map((rule, index) => (
+          <h3>{index + 1}.- {rule} </h3>
+        ))}
+      </div>
     </>
   )
 }
