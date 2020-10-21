e3ec8b6260c899993afa170a3bc37ebc7876f7d1
genaromartinez <genaro.mtz1998@gmail.com>
2020-10-21 12:24:06 -0500

    Modify role table to receive league roles

--- src/components/role-table/RoleTable.js
+++ src/components/role-table/RoleTable.js
@@ -2,42 +2,38 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
 import './RoleTable.css';
-import {useDocument} from "react-firebase-hooks/firestore";
-import firebase from "firebase";
+import { useCollectionDataOnce } from "react-firebase-hooks/firestore";
+import { RolesCollections } from "../../firestoreCollections";
+import { useParams } from "react-router-dom";
 
 function RoleTable() {
-
-  const [value, loading, error] = useDocument(
-    firebase.firestore().doc('role-table/sckS1z8gBIlPu75ypFT9'),
-    {
-      snapshotListenOptions: { includeMetadataChanges: false },
-    }
-  );
-
-  return(
+  const { leagueId } = useParams()
+  const [data, loading, error] = useCollectionDataOnce(
+    RolesCollections.where('leagueId', '==', leagueId || ''))
+  return (
     <Card className="table-responsive">
       <h2>Próximos encuentros</h2>
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
       {loading && <span>Collection: Loading...</span>}
-      {value && (
+      {data && (
         <Table responsive>
           <thead className="thead-dark">
-          <tr>
-            <th scope="col">Equipo A</th>
-            <th scope="col">Equipo B</th>
-            <th scope="col">Cancha</th>
-            <th scope="col">Hora</th>
-          </tr>
+            <tr>
+              <th scope="col">Equipo A</th>
+              <th scope="col">Equipo B</th>
+              <th scope="col">Cancha</th>
+              <th scope="col">Hora</th>
+            </tr>
           </thead>
           <tbody>
-          {value.data().roles.map((rol, index) =>
-            <tr className="tablerow" key={index}>
-              <td>{ rol.equipoA }</td>
-              <td>{ rol.equipoB }</td>
-              <td>{ rol.cancha }</td>
-              <td>{ rol.hora.toDate().toLocaleTimeString() }</td>
-            </tr>
-          )}
+            {data[0].roles && data[0].roles.map((rol, index) =>
+              <tr className="tablerow" key={index}>
+                <td>{rol.equipoA}</td>
+                <td>{rol.equipoB}</td>
+                <td>{rol.cancha}</td>
+                <td>{rol.hora}</td>
+              </tr>
+            )}
           </tbody>
         </Table>
       )}
