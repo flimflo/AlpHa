1bf1da7025afcd867c2b07d68ae9b10a7516779e
genaromartinez <genaro.mtz1998@gmail.com>
2020-10-22 16:20:32 -0500

    Add styles to sections

--- src/components/pages/media/Media.js
+++ src/components/pages/media/Media.js
@@ -3,16 +3,17 @@ import { Card } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { getStoragePath } from "../../../firebaseStorage";
 import { MediaCollection } from "../../../firestoreCollections";
+import './Media.css';
 
 function Media() {
-  const { leagueId } = useParams() 
+  const { leagueId } = useParams()
   const [data, setData] = useState([])
 
   useEffect(() => {
     const unsubscribe = MediaCollection.where('leagueId', '==', leagueId || '')
       .orderBy('date', 'desc')
       .onSnapshot(s => {
-        Promise.all(s.docs.map(async d => ({        
+        Promise.all(s.docs.map(async d => ({
           ...d.data(),
           pictureUrl: d.data().path && await getStoragePath(d.data().path),
           date: new Date(d.data().date.toMillis()).toLocaleDateString(),
@@ -23,18 +24,22 @@ function Media() {
     return unsubscribe
   }, [leagueId])
 
-  return(
+  return (
     <>
+      <div className="section">
       <h1>Multimedia</h1>
-      {data.map(d => (
-        <Card style={{ width: '18rem' }} className="m-4">
-        {d.pictureUrl && <Card.Img variant="top" src={d.pictureUrl} />}
-        <Card.Body>
-          <Card.Title>{d.title}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted" >{d.date}</Card.Subtitle>
-        </Card.Body>
-      </Card>
-      ))}
+        <div className="section-content media-table">
+          {data.map(d => (
+            <Card style={{ width: '18rem' }} className="m-4">
+              {d.pictureUrl && <Card.Img variant="top" src={d.pictureUrl} />}
+              <Card.Body>
+                <Card.Title>{d.title}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted" >{d.date}</Card.Subtitle>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
