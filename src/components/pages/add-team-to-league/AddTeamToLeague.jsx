e28c64c48c983ec770a1c121590996d317e53904
the-vala <diegoi.vala@gmail.com>
2020-10-27 18:31:02 -0600

    Initialized victories and goals stats in teams collection

--- src/components/pages/add-team-to-league/AddTeamToLeague.jsx
+++ src/components/pages/add-team-to-league/AddTeamToLeague.jsx
@@ -27,6 +27,10 @@ export function AddTeamToLeague() {
         await TeamsCollection.add({
             leagueId: leagueId,
             teamName: teamName,
+            won: 0,
+            lost: 0,
+            golesFavor: 0,
+            golesContra: 0,
             players: await Promise.all(players.map(async p => {
                 const timestamp = firestore.Timestamp.now().toMillis()
                 const path = `/${leagueId}/${timestamp}${p.picture[0].name}`
